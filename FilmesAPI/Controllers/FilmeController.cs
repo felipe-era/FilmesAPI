3c20ac20008e37aafacbfa9c21479eff9a2b193d
felipec <felipeedc29@gmail.com>
2023-06-01 17:37:50 -0300

    Commentando e identando

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -7,42 +7,63 @@ namespace FilmesAPI.Controllers;
 [Route("[controller]")]
 public class FilmeController : ControllerBase
 {
+    #region == Atributos ==
+
     private static List<Filme> filmes = new List<Filme>();
     private static int id = 0;
 
+    #endregion
+
     [HttpPost] //post
-    public void AdicionaFilme([FromBody]Filme filme) //[FromBody] é o que vem no corpo da requisição
+    public IActionResult AdicionaFilme([FromBody] Filme filme) //[FromBody] é o que vem no corpo da requisição
     {
         //por validações por region..
         //if ((!string.IsNullOrEmpty(filme.Titulo) && (!string.IsNullOrEmpty(filme.Genero)) && filme.Duracao >= 70))
         //{ } Não se utiliza validações aqui.. usa a data annotations la na classe filme no caso
         filme.Id = id++;
-        filmes.Add(filme); 
-        Console.WriteLine("Após o ADD e validações");
-        Console.WriteLine(filme.Titulo); 
-        Console.WriteLine(filme.Genero);
-        Console.WriteLine(filme.Duracao);
+        filmes.Add(filme);
+
+        //Padrão rest como é?
+        //R: deve-se retornar o objeto ao usuário (Retornar as informações que foram recém cadastradas (inseridas)
+        //
+        return CreatedAtAction(nameof(ConsultaFilmesPorId), //chama o método get para apresentar ao usuario
+                               new { id = filme.Id }, //parametro do método acima
+                               filme);
     }
 
-    [HttpGet] //get
-    public IEnumerable<Filme> ConsultaFilmes()
+    //[HttpGet] //consulta todos os filmes
+    //public IEnumerable<Filme> ConsultaFilmes()
+    //{
+    //    return filmes;
+    //}
+
+    //[HttpGet("{id}")] //quando tiver parâmetros é necessario adicionar ao get
+    //public Filme? ConsultaFilmesPorId(int id)
+    //{
+    //    return filmes.FirstOrDefault(filme => filme.Id == id);
+    //    //o endereço informado pelo usuário retornar o endereço informado pelo usuário.
+    //    //caso não tenha traz nulo
+    //    //Ex: para trazer o filme id 1 usar o get no Postman/insomnia
+    //    //https://localhost:7105/filme/1
+    //}
+
+    [HttpGet] //consulta filmes por intervalo skip and take 
+    //https://localhost:7105/filme?skip=5&take=2 pula 5 e pega os 2 primeiros \/ quando não informado deixa o valor padrão como 0 ou 2
+    public IEnumerable<Filme> ConsultaFilmesIntervalo([FromQuery] int skip = 0, [FromQuery] int take = 2)
     {
-        return filmes;
+        return filmes.Skip(skip).Take(take);
     }
 
-    [HttpGet("{id}")] //quando tiver parâmetros é necessario adicionar ao get
-    public Filme? ConsultaFilmesPorId(int id)
+    [HttpGet("{id}")] //
+    public IActionResult ConsultaFilmesPorId(int id)
     {
-        return filmes.FirstOrDefault(filme => filme.Id == id);
-        //o endereço informado pelo usuário retornar o endereço informado pelo usuário.
-        //caso não tenha traz nulo
-        //Ex: para trazer o filme id 1 usar o get no Postman/insomnia
-        //https://localhost:7105/filme/1
-    }
+        var objfilme = filmes.FirstOrDefault(filme => filme.Id == id);
 
-     
+        if (objfilme == null) return NotFound();
+
+        return Ok(objfilme);
+    }
 
 
 
 }
- 
