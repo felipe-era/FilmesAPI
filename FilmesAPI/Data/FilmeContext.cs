5b9d3aab86d871a1c800ec2384628c71355803e6
felipec <felipeedc29@gmail.com>
2023-06-02 10:20:34 -0300

    Ajustes p/ comunicar com bd + retirada wheater

---
+++ FilmesAPI/Data/FilmeContext.cs
@@ -0,0 +1,18 @@
+﻿using FilmesAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmesAPI.Data
+{
+    public class FilmeContext : DbContext //add o DbContext para usar o Entity
+    {
+
+        public FilmeContext(DbContextOptions<FilmeContext> opts) : base(opts)//ctor
+        {
+            
+        }
+
+        //propriedades para acesso aos filmes da base
+        public DbSet<Filme> Filmes { get; set; }
+
+    }
+}
