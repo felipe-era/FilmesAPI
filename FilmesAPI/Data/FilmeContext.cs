007a1d0a1496bbbbb75d1fe706b6b37cf8af2074
PC <PC@DESKTOP-F726TKR>
2023-06-03 22:02:32 -0300

    commentary edit

--- FilmesAPI/Data/FilmeContext.cs
+++ FilmesAPI/Data/FilmeContext.cs
@@ -4,8 +4,8 @@ using Microsoft.EntityFrameworkCore;
 namespace FilmesAPI.Data
 {
     //FilmeContext = classe de cotexto para acesso ao banco
-    // como se conecta e como acessa!?
-    // Abstrai a lógica de acesso ao banco de dados, reduz o esforço para acessar o BD
+    //como se conecta e como acessa!?
+    //Abstrai a lógica de acesso ao banco de dados, reduz o esforço para acessar o BD
     public class FilmeContext : DbContext //add o DbContext para usar o Entity
     {
         public FilmeContext(DbContextOptions<FilmeContext> opts) : base(opts)//ctor
