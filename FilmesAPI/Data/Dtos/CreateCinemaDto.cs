9651417374643baaf1e2688e78b1bf5c1e4f4812
PC <PC@DESKTOP-F726TKR>
2023-06-08 19:01:20 -0300

    One to One

--- FilmesAPI/Data/Dtos/CreateCinemaDto.cs
+++ FilmesAPI/Data/Dtos/CreateCinemaDto.cs
@@ -4,10 +4,7 @@ namespace FilmesAPI.Data.Dtos;
 
 public class CreateCinemaDto
 {
-    [Key]
-    [Required]
-    public int Id { get; set; }
-
     [Required(ErrorMessage = "O campo de nome é obrigatório")]
     public string Nome { get; set; }
+    public int EnderecoId { get; set; }
 }
