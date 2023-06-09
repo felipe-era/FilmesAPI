9735700b386cdea270174144106dbda8c5fff444
felipec <felipeedc29@gmail.com>
2023-06-09 13:56:58 -0300

    Ajuste MVC/Reorder

---
+++ FilmesAPI/Profiles/CinemaProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using FilmesAPI.Data.Dtos;
+
+namespace FilmesAPI.Models.Profiles;
+
+public class CinemaProfile : Profile
+{
+    public CinemaProfile()
+    {
+        CreateMap<CreateCinemaDto, Cinema>();
+        CreateMap<Cinema, ReadCinemaDto>().ForMember(cinemaDto => cinemaDto.Endereco,
+                                                           opt => opt.MapFrom(cinema => cinema.Endereco));
+        CreateMap<UpdateCinemaDto, Cinema>();
+    }
+}
